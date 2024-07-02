975c74446dad460f51f01b07fb8bdc71b096be50
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-07-02 14:18:27 +0100

    Update page.tsx

--- src/app/(routes)/admin/dashboard/page.tsx
+++ src/app/(routes)/admin/dashboard/page.tsx
@@ -113,7 +113,7 @@ const AdminDashboardPage: React.FC = () => {
       <div className="max-w-7xl mx-auto bg-white p-4 sm:p-8 rounded-lg shadow-md">
         <div className="flex flex-col md:flex-row justify-between mb-4 sm:mb-6">
           <h1 className="text-xl sm:text-2xl font-bold">Admin Dashboard</h1>
-          <div className="flex flex-col md:flex-row justify-between mt-3 md:mt-0 w-full md:w-[55%] my-auto space-x-4">
+          <div className="flex flex-col md:flex-row justify-between mt-3 md:mt-0 w-full md:w-[55%] my-auto space-x-4 md:space-y-4">
             <select
               value={selectedLevel}
               onChange={handleLevelChange}
